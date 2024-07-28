bfe21cc8404c4803716401a08ffc2c311d9fa246
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-07-28 19:28:54 -0400

    making changes to the database configuration

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,9 +1,11 @@
 //
+import pg from 'pg';
 import { Sequelize } from 'sequelize';
 import { POSTGRES_VERCEL_URL } from '../config.js';
 
 const sequelize = new Sequelize(POSTGRES_VERCEL_URL, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectModule: pg
 });
 
 export default sequelize;
