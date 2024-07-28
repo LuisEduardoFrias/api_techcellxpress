b48d068d92a8af33757ec0bc6b5495dc884d23fa
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-07-27 21:18:14 -0400

    configurations from vercel

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ export const {
   SWAGGER = process.env.SWAGGER || '/', //'/api-docs',
   DOMAIN = process.env.DOMAIN || 'localhost:',
   SECRET_ENCRYOT_KEY = process.env.SALT || 'tcx-crypto',
-  PORT = process.env.PORT || 3010,
-  ORIGIN = process.env.ORIGIN || ['http://localhost:3000','http://localhost:3010'],
+  PORT = process.env.PORT || 3000,
+  ORIGIN = process.env.ORIGIN || ['http://localhost:3000', 'http://localhost:3010'],
   METHODS = process.env.METHODS || ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || "3$t0EsUnC0d1guBonoS3cr3t0_p@r@-t0d0L4@p",
   POSTGRES_VERCEL_URL = process.env.POSTGRES_VERCEL_URL
